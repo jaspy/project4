0415f621016189df6d9da26f407d0feb531f68e8
foobic <foobic@gmail.com>
2019-03-10 22:34:17 +0200

    feat: docker-compose added

---
+++ 123.sql
@@ -0,0 +1,11 @@
+-- Adminer 4.7.1 PostgreSQL dump
+
+\connect "app";
+
+DROP TABLE IF EXISTS "123";
+CREATE TABLE "public"."123" (
+    "123" integer NOT NULL
+) WITH (oids = false);
+
+
+-- 2019-03-10 20:22:40.170552+00
